15ecdf3e94146446214b53af0eee809439639ea1
dgalanf01 <169030606+dgalanf01@users.noreply.github.com>
2024-05-23 13:35:07 +0200

    Create Prueba.java

---
+++ Prueba.java
@@ -0,0 +1,10 @@
+public class Prueba{
+
+  public static void main(String[] args) {
+		
+
+		System.out.println("Hola esto es la version 1.0 del programa de Pruebas, mi nombre no es Abraham, sino Daniel y vengo a por ti");
+
+
+	}
+}
