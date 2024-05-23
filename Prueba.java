158f3def1999517a04d9a3bf2929dfd06eb03c8a
Abraham Expósito García <169030329+aexpositog06@users.noreply.github.com>
2024-05-23 13:54:29 +0200

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -8,3 +8,41 @@ public class Prueba{
 
 	}
 }
+package Triangulo;
+
+class Triangulo {
+
+
+
+	    public static void main(String[] args) {
+
+	        System.out.println();
+
+	        for(int altura = 1; altura<=6; altura++){
+
+	            //Espacios en blanco
+
+	            for(int blancos = 1; blancos<=6-altura; blancos++){
+
+	                System.out.print(" ");
+
+	            }
+
+	 	            //Asteriscos
+
+	            for(int asteriscos=1; asteriscos<=(altura*2)-1; asteriscos++){
+
+	                System.out.print("*");
+
+	            }
+
+	            System.out.println();
+
+	        }
+	        System.out.println("Este es el programa de Rubén Marín ");
+
+	    }
+
+
+	}
+
